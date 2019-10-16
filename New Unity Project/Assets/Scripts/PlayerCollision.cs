c88c339b3f27a1339ea698685b189b27fa44ed4b
RuneVV <47038856+RuneVV@users.noreply.github.com>
2019-10-16 13:45:54 +0200

    Imported Damian's Prototype

---
+++ New Unity Project/Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    public PlayerMovement movement; //This is a reference top our PlayerMovement script
+
+    // This function runs when we hit another object.
+    // We get information about the collision and call it "collisionInfo".
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+        // We check if the object we collide with has a tag called "obstacle".
+        if (collisionInfo.collider.tag == "Obstacle")
+        {
+            movement.enabled = false; //This disables the players movement
+            FindObjectOfType<GameManager>().EndGame();
+        }
+    }
+}
---
+++ New Unity Project/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // This is a reference to the Rigidbody component called "rb"
+    public Rigidbody rb;
+
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 500f;
+
+    // Update is called once per frame
+    // Use FixedUpdate instead of Update when using physics (Makes it look smoother when things collide)
+    void FixedUpdate()
+    {
+        // Add a forward Force
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+        if (rb.position.y < -1f)
+        {
+            FindObjectOfType<GameManager>().EndGame();
+        }
+
+    }
+}
