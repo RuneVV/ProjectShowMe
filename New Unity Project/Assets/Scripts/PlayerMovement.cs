0648814c48c87c2007e5d2c1627b5dde7731aa9f
RuneVV <47038856+RuneVV@users.noreply.github.com>
2019-10-17 11:58:23 +0200

    Added that time increases overtime

--- New Unity Project/Assets/Scripts/PlayerMovement.cs
+++ New Unity Project/Assets/Scripts/PlayerMovement.cs
@@ -5,8 +5,16 @@ public class PlayerMovement : MonoBehaviour
     // This is a reference to the Rigidbody component called "rb"
     public Rigidbody rb;
 
-    public float forwardForce = 2000f;
-    public float sidewaysForce = 500f;
+    
+    public float forwardForce = 500f;
+    public float sidewaysForce = 100f;
+    public float increaseSpeed = 100f;
+    public float maxSpeed = 4000f;
+    public float currentspeed;
+
+    public bool go;
+
+
 
     private float minX = -3;
     private float maxX = 3;
@@ -15,7 +23,7 @@ public class PlayerMovement : MonoBehaviour
 
     private void Start()
     {
-        
+        forwardForce = 1000f;
     }
 
     private void Update()
@@ -23,13 +31,31 @@ public class PlayerMovement : MonoBehaviour
         Vector3 clampedPosition = transform.position;
         clampedPosition.x = Mathf.Clamp(clampedPosition.x, -7.0f, 7.0f);
         transform.position = clampedPosition;
+                        
     }
 
     // Update is called once per frame
     // Use FixedUpdate instead of Update when using physics (Makes it look smoother when things collide)
     void FixedUpdate() 
     {
-        
+       if(forwardForce <= maxSpeed)
+        {
+            go = true;
+        }
+        else
+        {
+            go = false;
+        }
+
+       if (go == true)
+        {
+            currentspeed = forwardForce += increaseSpeed * Time.deltaTime;
+        }
+        else
+        {
+            StopCounting();
+        }
+       
 
         // Add a forward Force
         rb.AddForce(0, 0, forwardForce * Time.deltaTime);
@@ -47,6 +73,12 @@ public class PlayerMovement : MonoBehaviour
         {
             FindObjectOfType<GameManager>().EndGame();
         }
+        
+    }
 
+   
+    public void StopCounting()
+    {
+        forwardForce = maxSpeed;
     }
 }
