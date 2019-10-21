084a9db45921ce26b994bafea0d3a4db33bfbe6e
RuneVV <47038856+RuneVV@users.noreply.github.com>
2019-10-21 13:21:28 +0200

    Added powerups and everything related to that

---
+++ New Unity Project/Assets/Scripts/PowerupsManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PowerupsManager : MonoBehaviour
+{
+    public static PowerupsManager Instance;
+
+    public int SeedCount;
+    public Text seedText;
+    public int maxSeeds = 5;
+
+    private void Awake()
+    {
+        Instance = this;
+        //seedText = GameObject.Find("SeedText").GetComponent<Text>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       if ( SeedCount == maxSeeds)
+        {
+            //counter naar 0
+            //power up telt af 10 - 0 , grote weer naar normaal.
+            
+        }
+    }
+
+    public void UpdateSeedCount(int _amount)
+    {
+        SeedCount += _amount;
+        seedText.text = SeedCount.ToString();
+    }
+}
---
+++ New Unity Project/Assets/SeedPrefab/Powerup.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Powerup : MonoBehaviour {
+    public float multiplier = 1.4f;
+
+    public int seedcount = 0;
+    public int currentSeed;
+    public int maxSeed = 5;
+
+    private GameObject pickupEffect;
+
+    public Text seedText;
+
+    private void Start()
+    {
+        
+
+    }
+    public void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter (Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Pickup(other);
+            
+        }
+    }
+    void Pickup(Collider player)
+    {
+
+        //Instantiate(pickupEffect, transform.position, transform.rotation);
+        
+
+        PowerupsManager.Instance.UpdateSeedCount(1);
+
+        player.transform.localScale *= multiplier;
+                
+        Destroy(gameObject);
+    }
+}
