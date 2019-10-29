c21e596a53309b7e57a44682f63678213f4a1563
Skoftig <saraderooij1@gmail.com>
2019-10-29 13:07:27 +0100

    ui

--- New Unity Project/Assets/Scripts/Menu.cs
+++ New Unity Project/Assets/Scripts/Menu.cs
@@ -7,4 +7,11 @@ public class Menu : MonoBehaviour
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
+
+    public void QuitGame()
+    {
+        Debug.Log("QUIT!!");
+        Application.Quit();
+    }
+
 }
